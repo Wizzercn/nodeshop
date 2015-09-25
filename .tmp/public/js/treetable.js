182aa439025c771b36c9fa1b577829ff53111be5
Wizzer <wizzer@qq.com>
2015-09-25 15:42:23 +0800

    del tmp

--- .tmp/public/js/treetable.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    $("head").append("<link rel=\"stylesheet\" href=\"/plugins/treetable/jquery.treetable.css\">");
-    $("body").append("<script type=\"text/javascript\" src=\"/plugins/treetable/jquery.treetable.js\"></script>");
-});
