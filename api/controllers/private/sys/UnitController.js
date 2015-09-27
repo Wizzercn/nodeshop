6e76ed880ff23990507df0e3cd689c8d91e6d23c
root <wizzer@qq.com>
2015-09-27 02:09:51 -0700

    detail unit

--- api/controllers/private/sys/UnitController.js
+++ api/controllers/private/sys/UnitController.js
@@ -4,27 +4,32 @@
 module.exports = {
   index: function (req, res) {
     var data = req.data;
-    Sys_unit.query('select * from sys_unit where length(path)=4 order by location asc,path asc',function (err, objs) {
-      var unit=[];
-      if(objs){
-        unit=objs;
+    Sys_unit.query('select * from sys_unit where length(path)=4 order by location asc,path asc', function (err, objs) {
+      var unit = [];
+      if (objs) {
+        unit = objs;
       }
-      data.unit=unit;
+      data.unit = unit;
       return res.view('private/sys/unit/index.ejs', data);
     });
 
   },
   child: function (req, res) {
     var data = req.data;
-    Sys_unit.query('select * from sys_unit where parentId = ? order by location asc,path asc',[parseInt(req.params.id)],function (err, objs) {
-      var unit=[];
-      if(objs){
-        unit=objs;
+    Sys_unit.query('select * from sys_unit where parentId = ? order by location asc,path asc', [parseInt(req.params.id)], function (err, objs) {
+      var unit = [];
+      if (objs) {
+        unit = objs;
       }
-      console.log('err:::'+JSON.stringify(err));
-      console.log('unit:::'+JSON.stringify(unit));
-      data.unit=unit;
+      data.unit = unit;
       return res.view('private/sys/unit/child.ejs', data);
+    });
+  },
+  detail: function (req, res) {
+    var data = req.data;
+    Sys_unit.findOne({id:req.params.id}).exec(function (err, obj) {
+      data.obj = obj;
+      return res.view('private/sys/unit/detail.ejs', data);
     });
   }
 };
