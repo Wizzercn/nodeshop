c31834aaad51b6d3016236e8ac810ce289f70d61
root <wizzer@qq.com>
2015-12-13 21:45:39 -0800

    b2c

---
+++ api/models/b2c/B2c_goods_spec_values.js
@@ -0,0 +1,36 @@
+/**
+ * Created by root on 11/4/15.
+ */
+var moment = require('moment');
+module.exports = {
+  schema: true,
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+    spec_value: {
+      type: 'string',
+      size: 100,
+      required: true
+    },
+    spec_alias: {
+      type: 'string',
+      size: 10
+    },
+    spec_picurl: {
+      type: 'string',
+      size: 255
+    },
+    location: {
+      type: 'integer'
+    },
+    specId: {
+      model: 'B2c_goods_specification',
+      index: true
+    }
+  }
+};
---
+++ api/models/b2c/B2c_goods_specification.js
@@ -0,0 +1,40 @@
+/**
+ * Created by root on 11/4/15.
+ */
+var moment = require('moment');
+module.exports = {
+  schema: true,
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  attributes: {
+    id: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: 'string',
+      size: 100,
+      required: true
+    },
+    type: {
+      type: 'string',
+      size: 10
+    },
+    memo: {
+      type: 'string',
+      size: 100
+    },
+    alias: {
+      type: 'string',
+      size: 100
+    },
+    location: {
+      type: 'integer'
+    },
+    articles: {
+      collection: 'B2c_goods_spec_values',
+      via: 'specId'
+    }
+  }
+};
