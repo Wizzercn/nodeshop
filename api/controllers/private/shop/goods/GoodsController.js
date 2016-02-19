e6d0f795356f6ddba3c189c9901687569888b375
Wizzer <wizzer@qq.com>
2016-02-18 19:37:41 -0800

    goods add

--- api/controllers/private/shop/goods/GoodsController.js
+++ api/controllers/private/shop/goods/GoodsController.js
@@ -14,5 +14,26 @@ module.exports = {
         return res.view('private/shop/goods/goods/add', req.data);
       });
     });
+  },
+  getClass:function(req,res){
+    var id=req.params.id;
+    Shop_goods_class.findOne(id).populate('typeid').exec(function (e, o) {
+      if(e)return res.json({code:1,msg:JSON.stringify(e)});
+      return res.json({code:0,msg:'',data:o});
+    });
+  },
+  getType:function(req,res){
+    var id=req.params.id;
+    Shop_goods_type.findOne(id).exec(function (e, o) {
+      if(e)return res.json({code:1,msg:JSON.stringify(e)});
+      return res.json({code:0,msg:'',data:o});
+    });
+  },
+  getProps:function(req,res){
+    var id=req.params.id;
+    Shop_goods_type_props.find({typeid:id}).sort({location:'asc'}).populate('values',{sort:{location:'asc'}}).exec(function (e, o) {
+      if(e)return res.json({code:1,msg:JSON.stringify(e)});
+      return res.json({code:0,msg:'',data:o});
+    });
   }
 };
