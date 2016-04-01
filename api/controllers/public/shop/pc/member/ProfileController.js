e0cb4e6a753fe791da16aa842feb80c8933f2571
root <wizzer@qq.com>
2016-04-01 07:48:12 -0700

    fix:member profile

--- api/controllers/public/shop/pc/member/ProfileController.js
+++ api/controllers/public/shop/pc/member/ProfileController.js
@@ -5,8 +5,8 @@ var StringUtil = require('../../../../../common/StringUtil');
 var moment = require('moment');
 module.exports = {
   index: function (req, res) {
-    var m=req.session.member;
-    if(!m|| m.memberId<1){
+    var m = req.session.member;
+    if (!m || m.memberId < 1) {
       return res.redirect('/login');
     }
     async.parallel({
@@ -22,54 +22,82 @@ module.exports = {
           done(null, list);
         });
       },
-      dbMember:function(done){
-        Shop_member.findOne(m.memberId).populate('lv_id').exec(function(e,o){
-          done(null,o);
+      dbMember: function (done) {
+        Shop_member.findOne(m.memberId).populate('lv_id').exec(function (e, o) {
+          done(null, o);
         });
       }
     }, function (err, result) {
       req.data.channelList = result.channelList || [];
       req.data.allClassList = result.allClassList || [];
-      req.data.dbMember=result.dbMember||{};
+      req.data.dbMember = result.dbMember || {};
       req.data.StringUtil = StringUtil;
       req.data.moment = moment;
-      req.data.siteTitle='基本资料_'+req.data.siteTitle;
+      req.data.siteTitle = '基本资料_' + req.data.siteTitle;
       return res.view('public/shop/' + sails.config.system.ShopConfig.shop_templet + '/pc/member_profile', req.data);
 
     });
   },
-  checkEmail:function(req,res){
+  checkEmail: function (req, res) {
     var member = req.session.member;
     if (!member || member.memberId < 1) {
       return res.json({code: 1, msg: '登录失效'});
     }
-    var email=req.body.email;
-    Shop_member.findOne({id:{'!':member.memberId},email:email}).exec(function(e,o){
-      if(o){
+    var email = req.body.email;
+    Shop_member.findOne({id: {'!': member.memberId}, email: email}).exec(function (e, o) {
+      if (o) {
         return res.json({code: 2, msg: ''});
-      }else {
+      } else {
         return res.json({code: 0, msg: ''});
       }
     });
   },
-  save:function(req,res){
+  save: function (req, res) {
     var member = req.session.member;
     if (!member || member.memberId < 1) {
       return res.json({code: 1, msg: '登录失效'});
     }
-    var wdate=req.body.wdate;
-    var b_year=0;
-    var b_month=0;
-    var b_day=0;
-    if(wdate){
-      b_year=moment(wdate).format('YYYY');
-      b_month=moment(wdate).format('MM');
-      b_day=moment(wdate).format('DD');
+    var wdate = req.body.wdate;
+    var b_year = 0;
+    var b_month = 0;
+    var b_day = 0;
+    if (wdate) {
+      b_year = moment(wdate).format('YYYY');
+      b_month = moment(wdate).format('MM');
+      b_day = moment(wdate).format('DD');
     }
-    Shop_member.findOne({id:{'!':member.memberId},email:req.body.email}).exec(function(e1,o1) {
-      if (o1) {
-        return res.json({code: 2, msg: ''});
-      }
+    if (req.body.email) {
+
+
+      Shop_member.findOne({id: {'!': member.memberId}, email: req.body.email}).exec(function (e1, o1) {
+        if (o1) {
+          return res.json({code: 2, msg: ''});
+        } else {
+          Shop_member.update(member.memberId, {
+            nickname: req.body.nickname, province: req.body.province,
+            city: req.body.city,
+            area: req.body.area,
+            addr: req.body.addr,
+            sex: req.body.sex,
+            realname: req.body.realname,
+            email: req.body.email,
+            b_year: b_year,
+            b_month: b_month,
+            b_day: b_day
+          }).exec(function (e2, o2) {
+            sails.log.debug('e2:::' + e2);
+            if (e2) {
+              return res.json({code: 1, msg: ''});
+            } else {
+              member.nickname = req.body.nickname || '';
+              req.session.member = member;
+              return res.json({code: 0, msg: ''});
+            }
+          });
+        }
+      });
+
+    } else {
       Shop_member.update(member.memberId, {
         nickname: req.body.nickname, province: req.body.province,
         city: req.body.city,
@@ -77,19 +105,19 @@ module.exports = {
         addr: req.body.addr,
         sex: req.body.sex,
         realname: req.body.realname,
-        email: req.body.email,
         b_year: b_year,
         b_month: b_month,
         b_day: b_day
       }).exec(function (e2, o2) {
+        sails.log.debug('e2:::' + e2);
         if (e2) {
           return res.json({code: 1, msg: ''});
         } else {
-          member.nickname=req.body.nickname||'';
-          req.session.member=member;
+          member.nickname = req.body.nickname || '';
+          req.session.member = member;
           return res.json({code: 0, msg: ''});
         }
       });
-    });
+    }
   }
 };
