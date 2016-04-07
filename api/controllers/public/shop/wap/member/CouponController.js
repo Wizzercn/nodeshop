6462c80bd7f735d89f3b0a1bdcb363532a7b7cc1
Wizzer <wizzer@qq.com>
2016-04-06 20:03:38 -0700

    wap sorce&coupon

--- api/controllers/public/shop/wap/member/CouponController.js
+++ api/controllers/public/shop/wap/member/CouponController.js
@@ -7,42 +7,19 @@ module.exports = {
   index: function (req, res) {
     var m=req.session.member;
     if(!m|| m.memberId<1){
-      return res.redirect('/login');
+      return res.redirect('/wap/login');
     }
-    async.parallel({
-      //获取cms栏目分类
-      channelList: function (done) {
-        Cms_channel.getChannel(function (list) {
-          done(null, list);
-        });
-      },
-      //获取所有商品分类
-      allClassList: function (done) {
-        Shop_goods_class.getAllClass(function (list) {
-          done(null, list);
-        });
-      },
-      dbMember:function(done){
-        Shop_member.findOne(m.memberId).populate('lv_id').exec(function(e,o){
-          done(null,o);
-        });
-      }
-    }, function (err, result) {
-      req.data.channelList = result.channelList || [];
-      req.data.allClassList = result.allClassList || [];
-      req.data.dbMember=result.dbMember||{};
-      req.data.StringUtil = StringUtil;
-      req.data.moment = moment;
-      return res.view('public/shop/' + sails.config.system.ShopConfig.shop_templet + '/wap/member_coupon', req.data);
+    req.data.StringUtil = StringUtil;
+    req.data.moment = moment;
+    return res.view('public/shop/' + sails.config.system.ShopConfig.shop_templet + '/wap/member_coupon', req.data);
 
-    });
   },
   list: function (req, res) {
     var member = req.session.member;
     if (!member || member.memberId < 1) {
       return res.json({code: 1, msg: ''});
     }
-    var pageSize = 16;
+    var pageSize = 5;
     var start = StringUtil.getInt(req.query.start);
     var sort = {status:'asc',createdAt: 'desc'};
     var where = {memberId: member.memberId};
--- api/controllers/public/shop/wap/member/ScoreController.js
+++ api/controllers/public/shop/wap/member/ScoreController.js
@@ -7,21 +7,9 @@ module.exports = {
   index: function (req, res) {
     var m=req.session.member;
     if(!m|| m.memberId<1){
-      return res.redirect('/login');
+      return res.redirect('/wap/login');
     }
     async.parallel({
-      //获取cms栏目分类
-      channelList: function (done) {
-        Cms_channel.getChannel(function (list) {
-          done(null, list);
-        });
-      },
-      //获取所有商品分类
-      allClassList: function (done) {
-        Shop_goods_class.getAllClass(function (list) {
-          done(null, list);
-        });
-      },
       dbMember:function(done){
         Shop_member.findOne(m.memberId).populate('lv_id').exec(function(e,o){
           done(null,o);
@@ -33,8 +21,6 @@ module.exports = {
         });
       }
     }, function (err, result) {
-      req.data.channelList = result.channelList || [];
-      req.data.allClassList = result.allClassList || [];
       req.data.couponList = result.couponList || [];
       req.data.dbMember=result.dbMember||{};
       req.data.StringUtil = StringUtil;
@@ -48,7 +34,7 @@ module.exports = {
     if (!member || member.memberId < 1) {
       return res.json({code: 1, msg: '登录失效'});
     }
-    var pageSize = 10;
+    var pageSize = 5;
     var start = StringUtil.getInt(req.query.start);
     var sort = {createdAt: 'desc'};
     var where = {memberId: member.memberId};
