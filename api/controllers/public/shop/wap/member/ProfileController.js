ca055503d936495c6dc2b17f731a731bde08c1f5
Wizzer <wizzer@qq.com>
2016-04-08 01:32:13 -0700

    wap member/profile

--- api/controllers/public/shop/wap/member/ProfileController.js
+++ api/controllers/public/shop/wap/member/ProfileController.js
@@ -7,29 +7,15 @@ module.exports = {
   index: function (req, res) {
     var m = req.session.member;
     if (!m || m.memberId < 1) {
-      return res.redirect('/login');
+      return res.redirect('/wap/login');
     }
     async.parallel({
-      //获取cms栏目分类
-      channelList: function (done) {
-        Cms_channel.getChannel(function (list) {
-          done(null, list);
-        });
-      },
-      //获取所有商品分类
-      allClassList: function (done) {
-        Shop_goods_class.getAllClass(function (list) {
-          done(null, list);
-        });
-      },
       dbMember: function (done) {
         Shop_member.findOne(m.memberId).populate('lv_id').exec(function (e, o) {
           done(null, o);
         });
       }
     }, function (err, result) {
-      req.data.channelList = result.channelList || [];
-      req.data.allClassList = result.allClassList || [];
       req.data.dbMember = result.dbMember || {};
       req.data.StringUtil = StringUtil;
       req.data.moment = moment;
@@ -66,8 +52,6 @@ module.exports = {
       b_day = moment(wdate).format('DD');
     }
     if (req.body.email) {
-
-
       Shop_member.findOne({id: {'!': member.memberId}, email: req.body.email}).exec(function (e1, o1) {
         if (o1) {
           return res.json({code: 2, msg: ''});
