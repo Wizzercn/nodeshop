78c296bedefd8dea6a5fe29b7d9792b5f4a58a2f
Wizzer <wizzer@qq.com>
2016-05-03 15:50:39 +0800

    ..

---
+++ api/controllers/private/shop/sales/CouponController.js
@@ -0,0 +1,75 @@
+/**
+ * Created by wizzer on 2016/5/3.
+ */
+var moment = require('moment');
+var StringUtil = require('../../../../common/StringUtil');
+module.exports = {
+  index: function (req, res) {
+    return res.view('private/shop/sales/coupon/index', req.data);
+  },
+  data: function (req, res) {
+    var pageSize = parseInt(req.body.length);
+    var start = parseInt(req.body.start);
+    var page = start / pageSize + 1;
+    var draw = parseInt(req.body.draw);
+    var order = req.body.order || [];
+    var columns = req.body.columns || [];
+    var sort = {};
+    var where = {};
+    if (order.length > 0) {
+      sort[columns[order[0].column].data] = order[0].dir;
+    }
+    Shop_sales_coupon.count(where).exec(function (err, count) {
+      if (!err && count > 0) {
+        Shop_sales_coupon.find(where)
+          .sort(sort)
+          .paginate({page: page, limit: pageSize})
+          .exec(function (err, list) {
+            return res.json({
+              "draw": draw,
+              "recordsTotal": pageSize,
+              "recordsFiltered": count,
+              "data": list
+            });
+          });
+      } else {
+        return res.json({
+          "draw": draw,
+          "recordsTotal": pageSize,
+          "recordsFiltered": 0,
+          "data": []
+        });
+      }
+    });
+  },
+  /**
+   * 启用
+   * @param req
+   * @param res
+   */
+  enable: function (req, res) {
+    var id = req.params.id;
+    Shop_sales_coupon.update({id: id}, {disabled: false}).exec(function (err, obj) {
+      if (err) {
+        return res.json({code: 1, msg: sails.__('update.fail')});
+      } else {
+        return res.json({code: 0, msg: sails.__('update.ok')});
+      }
+    });
+  },
+  /**
+   * 禁用
+   * @param req
+   * @param res
+   */
+  disable: function (req, res) {
+    var id = req.params.id;
+    Shop_sales_coupon.update({id: id}, {disabled: true}).exec(function (err, obj) {
+      if (err) {
+        return res.json({code: 1, msg: sails.__('update.fail')});
+      } else {
+        return res.json({code: 0, msg: sails.__('update.ok')});
+      }
+    });
+  }
+};
