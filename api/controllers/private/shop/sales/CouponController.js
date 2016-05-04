cd24051fea2611394c92b8631186ad76aeff475c
Wizzer <wizzer@qq.com>
2016-05-04 16:04:51 +0800

    优惠券记录

--- api/controllers/private/shop/sales/CouponController.js
+++ api/controllers/private/shop/sales/CouponController.js
@@ -119,5 +119,48 @@ module.exports = {
       return res.json({code: 0, msg: sails.__('delete.ok')});
 
     });
+  },
+  history: function (req, res) {
+    return res.view('private/shop/sales/coupon/history', req.data);
+  },
+  historyData: function (req, res) {
+    var pageSize = parseInt(req.body.length);
+    var start = parseInt(req.body.start);
+    var page = start / pageSize + 1;
+    var draw = parseInt(req.body.draw);
+    var memberId = req.body.memberId||'';
+    var order = req.body.order || [];
+    var columns = req.body.columns || [];
+    var sort = {};
+    var where = {};
+    if (order.length > 0) {
+      sort[columns[order[0].column].data] = order[0].dir;
+    }
+    if(memberId){
+      where.memberId=memberId;
+    }
+    Shop_member_coupon.count(where).exec(function (err, count) {
+      if (!err && count > 0) {
+        Shop_member_coupon.find(where)
+          .sort(sort)
+          .populate('memberId',{select:['id','nickname']})
+          .paginate({page: page, limit: pageSize})
+          .exec(function (err, list) {
+            return res.json({
+              "draw": draw,
+              "recordsTotal": pageSize,
+              "recordsFiltered": count,
+              "data": list
+            });
+          });
+      } else {
+        return res.json({
+          "draw": draw,
+          "recordsTotal": pageSize,
+          "recordsFiltered": 0,
+          "data": []
+        });
+      }
+    });
   }
 };
