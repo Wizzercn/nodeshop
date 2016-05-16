b065f90efacd55dd244626735dc268c019b4e525
Wizzer <wizzer@qq.com>
2016-05-16 17:30:46 +0800

    fix:前台订单微信支付关闭

--- config/local.js
+++ config/local.js
@@ -61,7 +61,7 @@ module.exports = {
    * to 80 (http://) or 443 (https://) if you have an SSL certificate        *
    ***************************************************************************/
 
-   port: process.env.PORT || 80,
+   port: process.env.PORT || 9080,
 
   /***************************************************************************
    * The runtime "environment" of your Sails app is either typically         *
