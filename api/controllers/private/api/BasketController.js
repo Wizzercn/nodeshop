0ca273b6576061c3e6d05e9e6b5a51017d207a4f
Wizzer <wizzer@qq.com>
2016-05-24 16:50:52 +0800

    site:seo

--- api/controllers/private/api/BasketController.js
+++ api/controllers/private/api/BasketController.js
@@ -51,15 +51,16 @@ module.exports = {
     });
   },
   add: function (req, res) {
+    req.data.appid = req.params.id;
     return res.view('private/api/basket/add', req.data);
   },
   addDo: function (req, res) {
     var body = req.body;
-        body.createdBy = req.session.user.id;
-        Api_basket.create(body).exec(function (e, o) {
-          if (e)return res.json({code: 1, msg: sails.__('add.fail')});
-          return res.json({code: 0, msg: sails.__('add.ok')});
-        });
+    body.createdBy = req.session.user.id;
+    Api_basket.create(body).exec(function (e, o) {
+      if (e)return res.json({code: 1, msg: sails.__('add.fail')});
+      return res.json({code: 0, msg: sails.__('add.ok')});
+    });
   },
   edit: function (req, res) {
     var id = req.params.id;
@@ -81,8 +82,51 @@ module.exports = {
       if (err) {
         return res.json({code: 1, msg: sails.__('delete.fail')});
       } else {
-        Api_basket_products.destroy({basketid: ids}).exec(function (e1) {});
+        Api_basket_products.destroy({basketid: ids}).exec(function (e1) {
+        });
         return res.json({code: 0, msg: sails.__('delete.ok')});
+      }
+    });
+  },
+  product: function (req, res) {
+    var id = req.params.id;
+    Api_basket.findOne(id).exec(function (e, o) {
+      req.data.obj = o || {};
+      return res.view('private/api/basket/product', req.data);
+    });
+  },
+  productData: function (req, res) {
+    var pageSize = parseInt(req.body.length);
+    var start = parseInt(req.body.start);
+    var page = start / pageSize + 1;
+    var draw = parseInt(req.body.draw);
+    var order = req.body.order || [];
+    var columns = req.body.columns || [];
+    var sort = {};
+    var where = {};
+    if (order.length > 0) {
+      sort[columns[order[0].column].data] = order[0].dir;
+    }
+    Api_basket_products.count(where).exec(function (err, count) {
+      if (!err && count > 0) {
+        Api_basket_products.find(where)
+          .sort(sort)
+          .paginate({page: page, limit: pageSize})
+          .exec(function (err, list) {
+            return res.json({
+              "draw": draw,
+              "recordsTotal": pageSize,
+              "recordsFiltered": count,
+              "data": list
+            });
+          });
+      } else {
+        return res.json({
+          "draw": draw,
+          "recordsTotal": pageSize,
+          "recordsFiltered": 0,
+          "data": []
+        });
       }
     });
   }
