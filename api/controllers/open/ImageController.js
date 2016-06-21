2582ecebdcdd210a5fccbc24d72a69e416434539
Wizzer <wizzer@qq.com>
2016-06-21 13:52:16 +0800

    图片缓存:304

--- api/controllers/open/ImageController.js
+++ api/controllers/open/ImageController.js
@@ -2,6 +2,7 @@
  * 通过Img_image表ID读取图片文件
  * Created by root on 1/18/16.
  */
+var moment = require("moment");
 module.exports = {
   /**
    * type: s 小图 m 中图 l 大图
@@ -10,12 +11,18 @@ module.exports = {
    * @returns {*}
    */
   file: function (req, res) {
+
     var id=req.params.id||'';
     var type=req.query.type||'';
     if(id.indexOf('.')>0){
       id=id.substring(0,id.indexOf('.'));
       Img_image.findOne(id).exec(function(e,o){
         if(o){
+          var modi=req.get('If-Modified-Since');
+          var up=moment(o.updatedAt*1000).toDate().toUTCString();
+          if(modi && modi== up){//缓存
+            return res.send(304);
+          }
           var path=o.src;
           if(type=='s'&& o.s_src){
             path=o.s_src;
@@ -24,6 +31,7 @@ module.exports = {
           }else if(type=='l'&& o.l_src){
             path=o.l_src;
           }
+          res.set('Last-Modified',up);
           return res.status(200).sendfile(sails.config.appPath+path);
         }else{
           return res.send(404,'not exist');
